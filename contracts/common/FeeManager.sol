dd5775efc7a4c1770c18f6ea38d8f0fbded3ef30
0xBriz <98397602+0xBriz@users.noreply.github.com>
2022-02-17 07:13:18 -0500

    setting base common contracts

---
+++ contracts/common/FeeManager.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.12;
+
+import "./StratManager.sol";
+
+abstract contract FeeManager is StratManager {
+    uint256 public constant STRATEGIST_FEE = 112;
+    uint256 public constant MAX_FEE = 1000;
+    uint256 public constant MAX_CALL_FEE = 111;
+
+    uint256 public constant WITHDRAWAL_FEE_CAP = 50;
+    uint256 public constant WITHDRAWAL_MAX = 10000;
+
+    uint256 public withdrawalFee = 10;
+
+    uint256 public callFee = 111;
+    uint256 public protocolFee = MAX_FEE - STRATEGIST_FEE - callFee;
+
+    function setCallFee(uint256 _fee) public onlyManager {
+        require(_fee <= MAX_CALL_FEE, "!cap");
+
+        callFee = _fee;
+        protocolFee = MAX_FEE - STRATEGIST_FEE - callFee;
+    }
+
+    function setWithdrawalFee(uint256 _fee) public onlyManager {
+        require(_fee <= WITHDRAWAL_FEE_CAP, "!cap");
+
+        withdrawalFee = _fee;
+    }
+}
